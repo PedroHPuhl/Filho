6a4cd8c5fcac0ac8a812b20191dd383101c7b1ca
Pedro Puhl <pedro.hp2255@gmail.com>
2023-12-13 16:52:05 -0300

    Second

---
+++ filho.go
@@ -0,0 +1,9 @@
+package filho
+
+func Filho() string {
+	return "Eu sou o filho!"
+}
+
+func Filha() string {
+	return "Eu sou a filha!"
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PedroHPuhl/Filho
+
+go 1.21.5
